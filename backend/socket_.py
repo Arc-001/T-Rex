0adeb04fb404b65f50d245d6b2a31e3b460e53d8
Arc-001 <25131347+Arc-001@users.noreply.github.com>
2025-04-27 13:23:04 +0530

    FEAT : ADDED Client socket and graceful server shutdown

--- backend/socket_.py
+++ backend/socket_.py
@@ -155,6 +155,27 @@ class socket_server:
         self.conn_semaphore.release()
         print(f"{addr} semaphore released")
 
+    def close_server(self):
+        #closing all the sockets
+        for addr, conn in self.addr_conn_dict.items():
+            try:
+                conn.shutdown(socket.SHUT_RDWR)
+            except Exception as e:
+                print(f"Unable to send the shutdown signal to {addr}\n\n Error : {e}")
+            finally:
+                conn.close()
+                print(f"Connection with {addr} closed")
+
+        #clearing dictionary
+        self.addr_conn_dict.clear()
+
+        #releasing all the semaphores
+        while self.conn_semaphore._value < self.max_listen:
+            self.conn_semaphore.release()
+
+
+
+
     def recv_message_daemon(self, addr):
         while True:
             conn = self.addr_conn_dict[addr]
@@ -185,5 +206,70 @@ class socket_server:
     def get_post_office(self):
         return self.post_office
     
+class socket_client:
+    def __init__(self, host = "localhost", port = 5000, data_len = 1024):
+        self.data_len = data_len
+        self.host = host
+        self.port = port
+        self.sock = socket.socket()
+        self.post_box = message_box()
+        self.establish_conn()
+
+    def establish_conn(self):
+        try:
+            self.sock.connect((self.host, self.port))
+            recv_thread = threading.Thread(target=self.recv_message_daemon)
+        except socket.error as e:
+            raise e
+        
+    def recv_message_daemon(self):
+        try:
+            while True:
+                data = self.sock.recv(self.data_len).decode()
+                if not data:
+
+                    #call the gracefull closing of connection
+
+                    break
+
+                self.post_box.add_recv_message(data)
+
+        except socket.error as e:
+            raise e
+        
+    def send_message(self, data:str):
+        try:
+            self.sock.send(data.encode())
+            self.message_box.add_send_message(data)
+        except Exception as e:
+            print(f"error sending the message: {data}\n error is : {e}")
+    
+    def close_conn(self):
+        try:
+            self.sock.shutdown(socket.SHUT_RDWR)
+        except Exception as e:
+            print("\n\n\n Error during shutdown of socket")
+        finally:
+            self.sock.close()
+            print("Socket closed")
+    
+    def get_recv_messages(self):
+        return self.post_box.get_recv_message()
+    
+    def get_sent_messages(self):
+        return self.post_box.get_send_message()
+    
+    def get_all_recv_messages(self):
+        return self.post_box.get_all_recv_messages()
+    
+    def get_all_sent_messages(self):
+        return self.post_box.get_all_send_messages()
+    
+    def get_post_box(self):
+        return self.post_box
+    
+
+
+
 
 
