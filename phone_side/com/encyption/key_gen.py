46d18190d2be68a3a9346a4f032a69b5d30aacb5
Arc-001 <25131347+Arc-001@users.noreply.github.com>
2025-05-09 13:51:58 +0530

    FEAT and FIX: Added the encryption and fixed the key class
    Encryption is a standalone function from key which can be used to encrypt messages we have a decrypt function for the vicce versa also fixed the missing self references in the key class

--- phone_side/com/encyption/key_gen.py
+++ phone_side/com/encyption/key_gen.py
@@ -1,5 +1,5 @@
 #importing key generator and unique id labler
-from pypg import PGPKey, PGPUID
+from pypg import PGPKey, PGPUID, PGPMessage
 from pypg.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm, SymmetricKeyAlgorithm, CompressionAlgorithm
 
 class key:
@@ -10,43 +10,102 @@ class key:
         self.password = password
 
 
-    def gen_key(uuid: str, hostname: str, password:str):
+    def gen_key(self,uuid: str, hostname: str, password:str):
         #make a new key object
-        key = PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 2048)
+        self.key = PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 2048)
         
         #makeing the lable for the key
         uid = PGPUID.new(uuid, hostname = hostname, password = password)
 
         #attaching the lable to the key
+        
+
+        '''
+        alternative compressions for future reference
+
+        CompressionAlgorithm.ZIP,
+        CompressionAlgorithm.ZLIB,
+        CompressionAlgorithm.BZ2,
+        CompressionAlgorithm.Uncompressed
+        '''
+        
         key.add_uid(
             uid,
             usage = [KeyFlags.Sign, KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage],
             hashes = [HashAlgorithm.SHA256],
             ciphers = [SymmetricKeyAlgorithm.AES256],
             compress = [CompressionAlgorithm.Uncompressed]
-            '''
-            alternative compressions for future reference
 
-            CompressionAlgorithm.ZIP,
-            CompressionAlgorithm.ZLIB,
-            CompressionAlgorithm.BZ2,
-            CompressionAlgorithm.Uncompressed
-            '''
         )
 
     def save_key(self, path: str):
 
         #save private key
         with open(path, "w") as key_file_private:
-            key_file_private.write(str(key))
+            key_file_private.write(str(self.key))
 
         #save the private key
         with open(f"{path}.pub", "w") as key_file_public:
-            key_file_public.write(str(key.pubkey))
+            key_file_public.write(str(self.key.pubkey))
 
     def get_key(self):
         return self.key
 
 
+# def get_key_path(path:str):
 
+#     return PGPKey.from_file(path)
+
+'''
+TODO:
+encrypt the private key by using 
+key.protect("your-strong-passphrase", SymmetricKeyAlgorithm.AES256, HashAlgorithm.SHA256)
+
+and unlock using 
+with my_key.unlock("password"):
+'''
+
+def encrypt_message_file(pub_key : str, message:str):
+    pub_key, info = PGPKey.from_file(pub_key)
+    info = dict(info)
+    # printing the key info
+    print("-----------------info in the key-----------------")
+    for key in info:
+        print (f"{key}: {info[key]}")
+
+    #create the message object to work with encryption in pypg
+    message = PGPMessage.new("This is a test to check the encryption")
+
+    #encrypting the message
+
+    encrypted = pub_key.encrypt(message)
+
+
+    #printing the encrypted message for debugging
+    print ("-----------------encrypted message-----------------")
+    print (f"encrypted message is {encrypted}")
+
+    return str(encrypted)
+    
+
+def decrypt_message(priv_key:str, enc_message:str):
+    private_key, info = PGPKey.from_file(priv_key)
+    info = dict(info)
+    # printing the key info
+    print("-----------------info in the key-----------------")
+    for key in info:
+        print (f"{key}: {info[key]}")
+
+    #making a message object to work with decryption in pypg
+    enc_msg = PGPMessage.from_blob(enc_message)
+
+    #decrypting the message
+    decrypted = private_key.decrypt(enc_msg)
+
+    #printing the decrypted message for debugging
+    print ("-----------------decrypted message-----------------")
+    print (f"decrypted message is {decrypted}")
+    
+
+    return str(decrypted)
 
